b396a2e33aead1be9af3d1cde1f6bc8758dd1073
Caio Matos <84683791+matos-caio@users.noreply.github.com>
2023-09-20 08:45:42 -0300

    Add files via upload

--- boilerplate-mochachai-main/tests/1_unit-tests.js
+++ boilerplate-mochachai-main/tests/1_unit-tests.js
@@ -4,9 +4,14 @@ const assert = chai.assert;
 suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
-    test('#isNull, #isNotNull', function () {
-      assert.isNull(null, 'This is an optional error description - e.g. null is null');
-      assert.isNotNull(1, '1 is not null');
+    describe('isNull and isNotNull functions', function() {
+      it('should return true when passed a null value', function() {
+        expect(isNull(null)).to.be.true;
+      });
+    
+      it('should return false when passed a null value', function() {
+        expect(isNotNull(null)).to.be.false;
+      });
     });
     // #2
     test('#isDefined, #isUndefined', function () {
